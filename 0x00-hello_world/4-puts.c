8939e3467b38c2be940b5a98ba3b52214e8c6e3b
Scottie0404 <osipitanadesegun@gmail.com>
2023-07-13 05:25:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/**
+ * main - A program that prints a line followed by a new line
+ * Return: 0 (good)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
