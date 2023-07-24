c3c470f2a3c91413d7fa0e2436e4ad56806814ca
scottie0404 <osipitanadesegun@gmail.com>
2023-07-24 19:20:45 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - A function that takes a pointer to and int ans update it
+ * Return: 0 on success
+ */
+
+int void main()
+{
+	int n;
+	int p;
+	n=10;
+	*p=&n;
+	*p=98;
+
+	return (0);
+}
